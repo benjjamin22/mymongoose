3a1583107f41ce9db084103ad6ce3644320dec22
benjamin <deeprising4ilife@gmail.com>
2024-07-11 11:14:43 -0800

    first commit

--- server.js
+++ server.js
@@ -151,9 +151,10 @@ app.get(["/", "/index.html"], (req, res) => {
 async function uploadImageToGoogleDrive(file) {
     const bufferStream = new stream.PassThrough();
     bufferStream.end(file.buffer);
-    const uuid = uuidv4();
+    //const uuid = uuidv4();
     const fileMetadata = {
-        name: uuid,
+        name: fileObject.originalname,
+        //name: uuid,
         parents: ["10KpoRo-jHT62ko_7BNH9khxA2S_6GY42"],
     };
 
