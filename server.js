13a35605f0e29615444844324b5a6d237182d12a
benjjamin22 <122483173+benjjamin22@users.noreply.github.com>
2024-10-02 07:54:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -215,9 +215,18 @@ app.post("/", upload.single('image'), async(req, res) => {
                 //const uuido = nanoid(8) + ud
           //  }
            // const uuid = ud
-        
+        function randomString(len) {
+        var str = "";                                // String result
+        for (var i = 0; i < len; i++) {              // Loop `len` times
+        var rand = Math.floor(Math.random() * 62); // random: 0..61
+        var charCode = rand += rand > 9 ? (rand < 36 ? 55 : 61) : 48; // Get correct charCode
+        str += String.fromCharCode(charCode);      // add Character to str
+         }
+        return str; // After all loops are done, return the concatenated string
+         }
+
             
-        const uuid = nanoid(10);
+        const uuid = randomString(10);
             
         let newNote = new Note({
             Aname: {
